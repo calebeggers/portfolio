3c3a916d1320911ec152211b12c51a35a2257d89
calebeggers <calebeggers11@gmail.com>
2016-09-07 18:23:35 -0400

    Added nav logic

--- src/js/controllers/layout.controller.js
+++ src/js/controllers/layout.controller.js
@@ -1,7 +1,24 @@
-function LayoutController () {
+import $ from "jquery"
 
+function LayoutController ($location, $rootScope) {
+	function stateCheck () {
+		let curr = $location.$$path;
+		if (curr === '/') {
+			$('#1').addClass('active');
+			$('#2,#3').removeClass('active');
+		} else if (curr === '/projects') {
+			$('#2').addClass('active');
+			$('#1,#3').removeClass('active');
+		} else if (curr === '/contact') {
+			$('#3').addClass('active');
+			$('#1,#2').removeClass('active');
+		}
+	};
+
+	stateCheck();
+	$rootScope.$on('$stateChangeStart', stateCheck);
 };
 
-LayoutController.$inject = [];
+LayoutController.$inject = ['$location', '$rootScope'];
 
 export { LayoutController }
